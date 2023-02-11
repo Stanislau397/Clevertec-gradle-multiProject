50f6f1d8f25854865b8cf9e170c8c495d38015f8
Stanislau Kachan <Lancer397@gmail.com>
2023-02-11 04:15:22 +0300

    refactor: Refactored CoreTestsReportPugin.groovy

--- buildSrc/src/main/groovy/ru/clevertec/testplugin/CoreTestsReportPlugin.groovy
+++ buildSrc/src/main/groovy/ru/clevertec/testplugin/CoreTestsReportPlugin.groovy
@@ -12,6 +12,7 @@ class CoreTestsReportPlugin implements Plugin<Project> {
     private static final String CORE_TESTS_REPORT_PATH = 'build/reports/tests/coreTests'
     private static final String CORE_TESTS = 'coreTests'
     private static final String TEST_REPORT = 'testReport'
+    private static final String CREATE_DIRECTORY = 'createTestReportDir'
     private static final String SEPARATOR = '/';
 
     @Override
@@ -20,22 +21,32 @@ class CoreTestsReportPlugin implements Plugin<Project> {
         project.tasks.register(TEST_REPORT, Copy) {
             dependsOn(CORE_TESTS)
             onlyIf {
-                project.rootProject.file(TEST_REPORTS_DIRECTORY).exists()
+                project.rootProject
+                        .file(TEST_REPORTS_DIRECTORY)
+                        .exists()
             }
+            def rootProjectTestReportsDirectory = project.getRootDir().toString() + SEPARATOR + TEST_REPORTS_DIRECTORY
             from CORE_TESTS_REPORT_PATH
-            into project.getRootDir().toString() + SEPARATOR + TEST_REPORTS_DIRECTORY
+            into rootProjectTestReportsDirectory
         }
 
         project.tasks.register(CORE_TESTS, Test).configure() {
+            dependsOn(CREATE_DIRECTORY)
             doFirst {
                 filter {
                     includeTestsMatching(CORE_TESTS_PATH)
                 }
             }
-            doLast {
-                if (!project.file(TEST_REPORTS_DIRECTORY).exists()) {
-                    project.rootProject.file(TEST_REPORTS_DIRECTORY).mkdir()
-                }
+        }
+
+        project.tasks.register(CREATE_DIRECTORY) {
+            def directoryExists = project.rootProject
+                    .file(TEST_REPORTS_DIRECTORY)
+                    .exists()
+            if (!directoryExists) {
+                project.rootProject
+                        .file(TEST_REPORTS_DIRECTORY)
+                        .mkdir()
             }
         }
     }
