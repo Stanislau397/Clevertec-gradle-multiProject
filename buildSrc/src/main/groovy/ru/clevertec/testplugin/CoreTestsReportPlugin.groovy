7e1eb1cb0907b4ed4f63d0949a8c5e4cc9576fe2
Stanislau Kachan <Lancer397@gmail.com>
2023-02-12 12:06:56 +0300

    add: deleteReportsDirectoryTask

--- buildSrc/src/main/groovy/ru/clevertec/testplugin/CoreTestsReportPlugin.groovy
+++ buildSrc/src/main/groovy/ru/clevertec/testplugin/CoreTestsReportPlugin.groovy
@@ -3,15 +3,18 @@ package ru.clevertec.testplugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.testing.Test
 
 class CoreTestsReportPlugin implements Plugin<Project> {
 
     private static final String TEST_REPORTS_DIRECTORY = 'test_report'
+    private static final String REPORTS_DIRECTORY = 'reports'
     private static final String CORE_TESTS_PATH = 'ru.clevertec.core*';
     private static final String CORE_TESTS_REPORT_PATH = 'build/reports/tests/coreTests'
     private static final String CORE_TESTS = 'coreTests'
     private static final String TEST_REPORT = 'testReport'
+    private static final String DELETE_REPORTS_DIRECTORY = 'deleteReportsDirectory'
     private static final String CREATE_DIRECTORY = 'createTestReportDir'
     private static final String SEPARATOR = '/';
 
@@ -19,15 +22,20 @@ class CoreTestsReportPlugin implements Plugin<Project> {
     void apply(Project project) {
 
         project.tasks.register(TEST_REPORT, Copy) {
+
             dependsOn(CORE_TESTS)
+
             onlyIf {
                 project.rootProject
                         .file(TEST_REPORTS_DIRECTORY)
                         .exists()
             }
+
             def rootProjectTestReportsDirectory = project.getRootDir().toString() + SEPARATOR + TEST_REPORTS_DIRECTORY
             from CORE_TESTS_REPORT_PATH
             into rootProjectTestReportsDirectory
+
+            finalizedBy(DELETE_REPORTS_DIRECTORY)
         }
 
         project.tasks.register(CORE_TESTS, Test).configure() {
@@ -48,6 +56,10 @@ class CoreTestsReportPlugin implements Plugin<Project> {
                         .file(TEST_REPORTS_DIRECTORY)
                         .mkdir()
             }
+        }
+
+        project.tasks.register(DELETE_REPORTS_DIRECTORY, Delete) {
+            delete project.layout.buildDirectory.dir(REPORTS_DIRECTORY)
         }
     }
 }
